258ad7aba01aaf3f1d09b4e33c30659b96df4c76
Jarwise <57717541+Jarwise@users.noreply.github.com>
2020-10-27 18:24:08 +0100

    Update CodeSearch.java

--- CodeSearch.java
+++ CodeSearch.java
@@ -3,10 +3,15 @@ public class CodeSearch {
     public static void main(String[] args){
         Scanner scany = new Scanner(System.in);
         String s = scany.nextLine();
-        int count = 0;
-        for(int i = 0; i < s.length()-3; i++){
-            if(s.charAt(i)=='c' && s.charAt(i+1)=='o' && s.charAt(i+3)=='e') count++;
-        }
+        int count = CountCode(s);
         System.out.println(count);
+    }
+
+    public static int CountCode(String str){
+        int count = 0;
+        for(int i = 0; i < str.length()-3; i++){
+            if(str.charAt(i)=='c' && str.charAt(i+1)=='o' && str.charAt(i+3)=='e') count++;
+        }
+        return(count);
     }
 }
